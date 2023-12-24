ecdb6579349986b9b2c0cfe8f762acd2ef9ed8be
Kaio Matos <kaio_matos22@hotmail.com>
2023-12-23 23:13:21 -0300

    rename and create main namespace

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import { Vortex } from "./packages/Vortex";
+import { VariableParser } from "./packages/VariableParser";
+
+const vortex_client = new Vortex.Client(new VariableParser(/__(\w{1,})__/g), {
+    variables: {
+        baseUrl: "https://dummyjson.com",
+    },
+});
+
+vortex_client.request({
+    path: "__baseUrl__/products",
+});
---
+++ app/packages/Vortex.ts
@@ -0,0 +1,28 @@
+import { VariableParser } from "./VariableParser";
+
+export namespace Vortex {
+    export interface Environment {
+        variables?: Record<string, string | number>;
+        headers?: Record<string, string | number>;
+    }
+
+    export interface RequestConfiguration extends RequestInit {
+        path: string;
+    }
+
+    export class Client {
+        parser: VariableParser;
+
+        constructor(
+            parser: VariableParser,
+            protected environment: Environment
+        ) {
+            this.parser = parser;
+            this.parser.setVariables(environment.variables ?? {});
+        }
+
+        request({ path, ...configuration }: RequestConfiguration) {
+            return fetch(this.parser.parse(path), configuration);
+        }
+    }
+}
