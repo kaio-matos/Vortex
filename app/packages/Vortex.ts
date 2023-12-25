6f7d80772ae9834c08d63d78212abbcefc98abe9
Kaio Matos <kaio_matos22@hotmail.com>
2023-12-25 19:08:56 -0300

    fix vortex headers

--- app/packages/Vortex.ts
+++ app/packages/Vortex.ts
@@ -5,7 +5,7 @@ import { argv, env } from "process";
 export namespace Vortex {
     export interface Configuration {
         variables?: Record<string, string | number>;
-        headers?: Record<string, string | number>;
+        headers?: HeadersInit;
     }
 
     export interface RequestConfiguration extends RequestInit {
@@ -36,19 +36,35 @@ export namespace Vortex {
     }
 
     export class Client {
+        headers?: HeadersInit;
+
         constructor(
             protected parser: VariableParser,
             protected config?: Configuration
         ) {
+            this.headers = config?.headers;
+
             this.parser.setVariables(config?.variables ?? {});
         }
 
         request({ path, ...configuration }: RequestConfiguration) {
             if (path instanceof URL) {
-                return fetch(this.parser.parse(path.href), configuration);
+                return fetch(this.parser.parse(path.href), {
+                    ...configuration,
+                    headers: {
+                        ...this.config?.headers,
+                        ...configuration.headers,
+                    },
+                });
             }
 
-            return fetch(this.parser.parse(path), configuration);
+            return fetch(this.parser.parse(path), {
+                ...configuration,
+                headers: {
+                    ...this.config?.headers,
+                    ...configuration.headers,
+                },
+            });
         }
     }
 }
